d08efe1c52a085a4a9c80a7eb56184178da9d290
Dan <vatolin_denis@mail.ru>
2022-08-29 21:26:02 +0700

    10 & 13 Examples

---
+++ Example10/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Input three-digit integer number: ");
+int n =Convert.ToInt32 (Console.ReadLine());
+
+n = n % 100;
+n = n / 10;
+
+
+Console.WriteLine(n);
