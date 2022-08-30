23d50bc2b7c74eb9240ec5689e97f0c3d5aabce0
unknown <@DESKTOP-MD3URQL.ortpc.kem>
2022-08-30 19:44:58 +0700

    Example27

---
+++ Example27/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+
+System.Console.WriteLine("Please input integer number: ");
+int summ = ConvertToInt32(Console.ReadLine());
+int i[0];
+int length = summ;
+while (lengt < 9);
+{
+summ = i + 1
+}
+System.Console.WriteLine(summ);
