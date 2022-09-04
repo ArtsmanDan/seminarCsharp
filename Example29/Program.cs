cc738b5d08339da3be86dfdb683891d71574abc3
unknown <@DESKTOP-MD3URQL.ortpc.kem>
2022-09-04 22:50:03 +0700

    Example 29

--- Example29/Program.cs
+++ Example29/Program.cs
@@ -1,34 +1,46 @@
 ﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
-
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
-
 6, 1, 33 -> [6, 1, 33]
 */
 
+string textNumbers = TextNumbersFromConsole();
 
-string textNumbers = String.Empty;
-
-System.Console.WriteLine("Введите цифру для индекса 0");
-textNumbers = Console.ReadLine();
-
-for (int i = 1; true; i++)
-{
-    System.Console.WriteLine($"Введите цифру для индекса {i}, чтобы закончить ввод оставте строчку пустой");
-    string number = Console.ReadLine();
-    if (number == "") break;
-    textNumbers = textNumbers + ", " + number;
-}
 System.Console.Write(textNumbers);
+System.Console.Write(" -> ");
+PrintArray(CreateArrayFromText(textNumbers));
 
-string[] text = textNumbers.Split(", ");
 
-System.Console.Write(" -> [");
-
-foreach (var item in text)
+string[] CreateArrayFromText(string textNumbers)
 {
-    System.Console.Write(item + ", ");
+    string[] text = textNumbers.Split(", ");
+    return text;
 }
-int origRow = Console.CursorTop;
-int origCol = Console.CursorLeft;
-Console.SetCursorPosition(origCol - 2, origRow);
-System.Console.WriteLine("]");
+
+string TextNumbersFromConsole()
+{
+    string textNumbers = String.Empty;
+    System.Console.WriteLine("Введите цифру для индекса 0");
+    textNumbers = Console.ReadLine();
+
+    for (int i = 1; true; i++)
+    {
+        System.Console.WriteLine($"Введите цифру для индекса {i}, чтобы закончить ввод оставте строчку пустой");
+        string number = Console.ReadLine();
+        if (number == "") break;
+        textNumbers = textNumbers + ", " + number;
+    }
+    return textNumbers;
+}
+
+void PrintArray(string[] array)
+{
+    System.Console.Write("[");
+    foreach (var item in array)
+    {
+        System.Console.Write(item + ", ");
+    }
+    int origRow = Console.CursorTop;
+    int origCol = Console.CursorLeft;
+    Console.SetCursorPosition(origCol - 2, origRow);
+    System.Console.WriteLine("]");
+}
