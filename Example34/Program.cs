3a9c047c8e71c09ba170bc6c588db3b3c74311bc
Dan <vatolin_denis@mail.ru>
2022-09-06 00:02:53 +0700

    Example34.36

--- Example34/Program.cs
+++ Example34/Program.cs
@@ -4,14 +4,23 @@
 
 [345, 897, 568, 234] -> 2
 */
+int min = 100;
+int max = 999;
+int length = 4;
 
-int[] BildingArray(int min, int max)
+int[] array = BildingArray(min, max, length);
+PrintArray(array);
+System.Console.Write(" -> ");
+System.Console.WriteLine(CountEvenNumbers(array));
+
+
+int[] BildingArray(int min, int max, int lengthArray)
 {
     Random rnd = new Random();
-    int[] array = new int[4];
+    int[] array = new int[lengthArray];
     for (int i = 0; i < array.Length; i++)
     {
-       array[i] = rnd.Next(min, max);
+       array[i] = rnd.Next(min, max + 1);
     }
     return array;
 }
@@ -26,15 +35,26 @@ void PrintArray(int[] array)
     int origRow = Console.CursorTop;
     int origCol = Console.CursorLeft;
     Console.SetCursorPosition(origCol - 2, origRow);
-    System.Console.WriteLine("]");
+    System.Console.Write("]");
 }
 
 bool IsEven(int x)
 {
-    bool s = true;
+    bool s = false;
     if (x % 2 == 0) s = true;
-    else s = false;
 
     return s;
 
+}
+
+int CountEvenNumbers(int[] array)
+{
+    int x = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (IsEven(array[i])) x++;
+    }
+
+    return x;
 }
