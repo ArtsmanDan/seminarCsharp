6f429921162a5dab808cbec6220e560dca756fa5
Dan <vatolin_denis@mail.ru>
2022-09-26 00:14:59 +0700

    Example 52-68

--- Example60/Program.cs
+++ Example60/Program.cs
@@ -7,3 +7,37 @@
 27(0,0,1) 90(0,1,1)
 26(1,0,1) 55(1,1,1)
 */
+
+PrintMatrix(FillArray(10, 9, 5, 2));
+
+int[,,] FillArray(int minValue, int n, int m, int z){
+    int[,,] array = new int[n, m, z];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = minValue;
+                minValue++;
+            }
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+      for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                System.Console.Write(matrix[i, j, k]);
+                System.Console.Write($"({i}, {j}, {k}) ");
+            }
+        }
+        System.Console.WriteLine();
+    }
+}
--- Example62/Program.cs
+++ Example62/Program.cs
@@ -5,3 +5,42 @@
 11 16 15 06
 10 09 08 07
 */
+
+int n = 10;
+int min = 100;
+
+PrintMatrix(FillMatrixCircle(n, min));
+
+int[,] FillMatrixCircle(int n, int min)
+{
+    int[,] array = new int[n, n];
+    int i = 0, j = 0;
+    while (n != 0)
+    {
+        int k = 0;
+        do { array[i, j++] = min++; } while (++k < n - 1);
+        for (k = 0; k < n - 1; k++) array[i++, j] = min++;
+        for (k = 0; k < n - 1; k++) array[i, j--] = min++;
+        for (k = 0; k < n - 1; k++) array[i--, j] = min++;
+
+        ++i; ++j;
+        if (n < 2) n = 0;
+        else n = n - 2;
+    }
+    return array;
+}
+
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        System.Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + " ");
+        }
+        System.Console.WriteLine("]");
+    }
+}
